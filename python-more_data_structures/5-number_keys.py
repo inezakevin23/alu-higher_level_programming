8bb24adf669451d7867f7e4981c207b8e4a5f073
Ineza Kevin <k.ineza1.alustudent.com>
2025-06-26 13:20:50 -0700

    completed

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    sum_keys = 0
-    for key, value in a_dictionary.items():
-        sum_keys += 1
-
-    return int(sum_keys)
-
+    return len(a_dictionary)
