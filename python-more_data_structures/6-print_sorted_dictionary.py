be75bffa5c772dea709e84bb6bd2889b1804183d
Ineza Kevin <k.ineza1.alustudent.com>
2025-06-26 13:09:56 -0700

    completed

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print(f'{key}: {value}')o
-
+    for K, V in sorted(a_dictionary.items()):
+        print("{}: {}".format(K, V))
