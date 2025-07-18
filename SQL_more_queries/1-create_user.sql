dd318f1ef27614c1450e5099b8eb3b2c53c600a6
Ineza Kevin <k.ineza1.alustudent.com>
2025-07-18 03:58:40 -0700

    updated

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,8 @@
--- A script that creates the MYSQL server user.
+-- Create user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on everything
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes immediately
+FLUSH PRIVILEGES;
