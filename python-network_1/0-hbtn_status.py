8716da0dedbf142eae30b622555f27fe2ee7ddfc
inezakevin23 <k.ineza1@alustudent.com>
2025-07-30 08:23:48 -0700

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -4,6 +4,12 @@
 import urllib.request
 
 url = 'https://alu-intranet.hbtn.io/status'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
+    '\n    Chrome/99.0.4844.84 Safari/537.36',
+}
+req = urllib.request.Request(url, headers=headers)
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
