c993afb88a11922efde62271e6abbf076332c7b9
inezakevin23 <k.ineza1@alustudent.com>
2025-07-30 08:35:49 -0700

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """Fetches https://alu-intranet.hbtn.io/status using urllib"""
+if __name__ == '__main__':
+	import urllib.request
 
-import urllib.request
+	url = 'https://alu-intranet.hbtn.io/status'
+	headers = {
+   	 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+   	 '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
+  	  '\n    Chrome/99.0.4844.84 Safari/537.36',
+	}
+	req = urllib.request.Request(url, headers=headers)
 
-url = 'https://alu-intranet.hbtn.io/status'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
-}
-req = urllib.request.Request(url, headers=headers)
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
+ 	with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        	content = res.read()
+        	print("Body response:")
+        	print("\t- type: {}".format(type(content)))
+        	print("\t- content: {}".format(content))
+        	print("\t- utf8 content: {}".format(content.decode('utf-8')))
